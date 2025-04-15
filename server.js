3deedaf70dc2bd9fc1b8da3ac45c0edc649b59fa
Alex <lexus190594@gmail.com>
2025-04-15 16:35:47 +0300

    1st commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(cors({
-  origin: ['https://todo-client2.vercel.app', 'http://localhost:5173'],
+  origin: 'http://localhost:5173',
   methods: ['POST', 'GET', 'PUT', 'DELETE'],
 }));
 
